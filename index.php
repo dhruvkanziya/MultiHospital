43284af0feecbe942dafa06d85a05f67be69ccc8
Kanziya Dhruvkumar <138643540+dhruvkanziya@users.noreply.github.com>
2025-07-11 08:20:36 +0530

    Update index.php

--- index.php
+++ index.php
@@ -21,6 +21,8 @@
 define('ENVIRONMENT', 'production');
 error_reporting(E_ALL);
 ini_set('display_errors', 1);
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
 
 /*
  *---------------------------------------------------------------
